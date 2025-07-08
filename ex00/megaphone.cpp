e1428094d811420117d1cde100b650509a37ef4e
yuu-ux <yuugaehara@gmail.com>
2025-07-08 17:36:19 +0900

    :wrench: 不要な改行を削除

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <algorithm>
-using namespace std;
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
-        return 0;
-    }
-    for (int i = 1; i < argc; i++) {
-        string input = argv[i];
-        transform(input.begin(), input.end(), input.begin(), ::toupper);
-        cout << input << endl;
-    }
-    return 0;
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++) {
+		std::string input = argv[i];
+		transform(input.begin(), input.end(), input.begin(), ::toupper);
+		std::cout << input;
+	}
+	std::cout << std::endl;
+	return 0;
 }
